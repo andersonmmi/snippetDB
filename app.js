9284de85d8fc010a4f09c224e63dc11f69e11e93
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-01 21:51:48 -0400

    App listens on 3000

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bluebird = require('bluebird');
 mongoose.Promise = require('bluebird');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 const mongoURL = 'mongodb://localhost:27017/snippet_creators';
+const app = express();
+
+//build app.listen here
+ app.listen(3000, function() {
+   console.log('Listening on port 3000')
+ });
