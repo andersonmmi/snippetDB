4f2d473a7b2d6d5f0f5e6de791bfb769bb990258
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-01 21:59:45 -0400

    Index renders

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
 const mongodb = require('mongodb');
 const mongoURL = 'mongodb://localhost:27017/snippet_creators';
 const app = express();
-mongosse.connect(mongoURL);
+mongoose.connect(mongoURL);
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -14,8 +15,11 @@ app.set('view engine', 'mustache')
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function(req,res){
+   res.render('index');
+});
 
 //build app.listen here
- app.listen(3000, function() {
+app.listen(3000, function() {
    console.log('Listening on port 3000')
- });
+});
