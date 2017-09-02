b775f5966524a70f8415b0fda8c981e676c9a136
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-01 21:34:43 -0400

    Require body-parser, mongodb

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bluebird = require('bluebird');
 mongoose.Promise = require('bluebird');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
