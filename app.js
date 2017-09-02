3c8facb039685612dc7491fe7feeaa498a7054fc
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-01 21:36:47 -0400

    mongoURL set to mongodb://localhost:27017/snippet_creators

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ const bluebird = require('bluebird');
 mongoose.Promise = require('bluebird');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
+const mongoURL = 'mongodb://localhost:27017/snippet_creators';
